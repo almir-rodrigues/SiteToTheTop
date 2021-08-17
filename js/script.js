2ef66c69c40204259376ebe7f0cc929b5eef397a
Almir Rodrigues <almir.srodrigues08@gmail.com>
2021-08-17 14:01:36 -0300

    add testimonials section

--- js/script.js
+++ js/script.js
@@ -35,3 +35,21 @@ icon.onclick = function(){
         arrow.className = "fa fa-angle-up"
     }
 }
+
+
+
+
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 2,
+      slideShadows: true,
+    },
+    loop: true,
+  });
