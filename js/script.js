29ce6fb16d026b02b84b3c1e48ef54a56d6c71e7
Almir Rodrigues <almir.rodrigues@cienciasnauticas.org.br>
2021-08-21 00:22:34 -0300

    added new FAQ section and small fixs

--- js/script.js
+++ js/script.js
@@ -1,27 +1,27 @@
 //Transição do nav menu no evento de scroll da pagina
 var srcInicial = "assets/images/logolight.png";
 var srcFinal = "assets/images/logodark.png"
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if ($(window).scrollTop() > 70){
-           $(".nav").addClass("scrolled");
-           $("#nav-logo").attr("src", srcInicial );
-           $(".nav-menu-option").css("color", "#fff");
-           $(".navbar-social").find("span").css("color", "#fff");
-           $(".navbar-social").find("a").css("color", "#fff");
-           $(".login-bnt").css("color", "#fff"); 
-           $(".login-bnt").css("border", "1px solid #fff");        
-        } 
-        else if ($(window).scrollTop() < 70) {
-           $(".nav").removeClass("scrolled");
-           $("#nav-logo").attr("src", srcFinal);
-           $(".nav-menu-option").css("color", "#000");
-           $(".navbar-social").find("span").css("color", "#000");
-           $(".navbar-social").find("a").css("color", "#000");
-           $(".login-bnt").css("color", "#000"); 
-           $(".login-bnt").css("border", "1px solid #000");    
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 70) {
+            $(".nav").addClass("scrolled");
+            //    $("#nav-logo").attr("src", srcInicial );
+            $(".nav-menu-option").css("color", "#fff");
+            $(".navbar-social").find("span").css("color", "#fff");
+            $(".navbar-social").find("a").css("color", "#fff");
+            $(".login-bnt").css("color", "#fff");
+            $(".login-bnt").css("border", "1px solid #fff");
         }
-   });
+        else if ($(window).scrollTop() < 70) {
+            $(".nav").removeClass("scrolled");
+            //    $("#nav-logo").attr("src", srcFinal);
+            $(".nav-menu-option").css("color", "#000");
+            $(".navbar-social").find("span").css("color", "#000");
+            $(".navbar-social").find("a").css("color", "#000");
+            $(".login-bnt").css("color", "#000");
+            $(".login-bnt").css("border", "1px solid #000");
+        }
+    });
 });
 
 
@@ -107,9 +107,8 @@ $(document).ready(function(){
 
 //Função para abrir e fechar os Sub-Menu adicionando e removendo a class "smenu-show",
 //com troca na aparecencia do menu selecionado através das classes "smenu-selected" e "fa-angle-up"
-
 //Sub-menu "Empréstimos"
-$('#icon-1').on('click', function(){
+$('#icon-1').on('click', function () {
     $('.sub-menu-1').toggleClass("smenu-show");
     $('.sub-menu-2').removeClass("smenu-show");
     $('.sub-menu-3').removeClass("smenu-show");
@@ -118,14 +117,14 @@ $('#icon-1').on('click', function(){
     $('#icon-2').removeClass("smenu-selected");
     $('#icon-3').removeClass("smenu-selected");
 })
-$('#close-arrow').on('click', function(){
+$('#close-arrow').on('click', function () {
     $('.sub-menu-1').removeClass("smenu-show");
     $('#emprestimos-arrow').toggleClass("fa-angle-up");
     $('#icon-1').removeClass("smenu-selected");
 })
 
 //Sub-menu "investimentos"
-$('#icon-2').on('click', function(){
+$('#icon-2').on('click', function () {
     $('.sub-menu-1').removeClass("smenu-show");
     $('.sub-menu-2').toggleClass("smenu-show");
     $('.sub-menu-3').removeClass("smenu-show");
@@ -134,14 +133,14 @@ $('#icon-2').on('click', function(){
     $('#icon-1').removeClass("smenu-selected");
     $('#icon-3').removeClass("smenu-selected");
 })
-$('#close-arrow-2').on('click', function(){
+$('#close-arrow-2').on('click', function () {
     $('.sub-menu-2').removeClass("smenu-show");
     $('#investimentos-arrow').toggleClass("fa-angle-up");
     $('#icon-2').removeClass("smenu-selected");
 })
 
 //Sub-menu "Créditos"
-$('#icon-3').on('click', function(){
+$('#icon-3').on('click', function () {
     $('.sub-menu-1').removeClass("smenu-show");
     $('.sub-menu-2').removeClass("smenu-show");
     $('.sub-menu-3').toggleClass("smenu-show");
@@ -150,11 +149,28 @@ $('#icon-3').on('click', function(){
     $('#icon-1').removeClass("smenu-selected");
     $('#icon-2').removeClass("smenu-selected");
 })
-$('#close-arrow-3').on('click', function(){
+$('#close-arrow-3').on('click', function () {
     $('.sub-menu-3').removeClass("smenu-show");
     $('#creditos-arrow').toggleClass("fa-angle-up");
     $('#icon-3').removeClass("smenu-selected");
 })
+
+//Função para fechar o sub-menu clicando fora do elemento aberto
+$(document).on('click', function (event) {
+    if (!$(event.target).closest('#icon-1, #sub-menu-1').length) {
+        $('.sub-menu-1').removeClass("smenu-show");
+        $('#emprestimos-arrow').removeClass("fa-angle-up");
+    }
+    if (!$(event.target).closest('#icon-2, #sub-menu-2').length) {
+        $('.sub-menu-2').removeClass("smenu-show");
+        $('#investimentos-arrow').removeClass("fa-angle-up");
+    }
+    if (!$(event.target).closest('#icon-3, #sub-menu-3').length) {
+        $('.sub-menu-3').removeClass("smenu-show");
+        $('#creditos-arrow').removeClass("fa-angle-up");
+    }
+});
+
 
 
 
@@ -163,8 +179,8 @@ var sideItems = document.getElementById("navbar");
 
 sideItems.style.right = "-360px";
 
-sideMenu.onclick = function(){
-    if (sideItems.style.right == "-360px"){
+sideMenu.onclick = function () {
+    if (sideItems.style.right == "-360px") {
         sideItems.style.right = "0"
     }
     else {
@@ -181,11 +197,46 @@ var swiper = new Swiper(".mySwiper", {
     centeredSlides: true,
     slidesPerView: "auto",
     coverflowEffect: {
-      rotate: 0,
-      stretch: 0,
-      depth: 100,
-      modifier: 2,
-      slideShadows: true,
+        rotate: 0,
+        stretch: 0,
+        depth: 100,
+        modifier: 2,
+        slideShadows: true,
     },
     loop: true,
-  });
+});
+
+
+
+
+
+//Função de manipulação do Accordion FAQ
+let toggles = document.getElementsByClassName('toggle');
+let contentDiv = document.getElementsByClassName('faq-content');
+let icons = document.getElementsByClassName('icon');
+
+for (let i = 0; i < toggles.length; i++) {
+    toggles[i].addEventListener('click', () => {
+        if (parseInt(contentDiv[i].style.height) != contentDiv[i].scrollHeight) {
+            contentDiv[i].style.height = contentDiv[i].scrollHeight + "px";
+            toggles[i].style.color = "#1880F1";
+            icons[i].classList.remove('fa-plus');
+            icons[i].classList.add('fa-minus');
+        }
+        else {
+            contentDiv[i].style.height = "0px";
+            toggles[i].style.color = "#134079";
+            icons[i].classList.remove('fa-minus');
+            icons[i].classList.add('fa-plus');
+        }
+
+        for (let j = 0; j < contentDiv.length; j++) {
+            if (j !== i) {
+                contentDiv[j].style.height = "0px";
+                toggles[j].style.color = "#134079";
+                icons[j].classList.remove('fa-minus');
+                icons[j].classList.add('fa-plus');
+            }
+        }
+    })
+}
